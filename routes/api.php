b3960dba142484ee8f96abb02cbf6cedb6df0d13
Andrade <daniloasf@outlook.com>
2024-01-22 15:05:09 -0300

    Adicionar Autenticação via Token para o usuário

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
 
-Route::apiResource('/users', UserController::class);
+/**
+ * Auth and Register Routes
+ */
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/users', UserController::class);
+});
+
+Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::get('/', function () {
     return response()->json(['message' => 'Ok']);
